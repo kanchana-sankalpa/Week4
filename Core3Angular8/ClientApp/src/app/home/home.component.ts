4d4a61a7a6ad324a4d248de6aedf3ef97471680b
Catchme <mdksankalpa@gmail.com>
2020-08-19 09:02:40 +0930

    List Students chagnes client home.components

--- Core3Angular8/ClientApp/src/app/home/home.component.ts
+++ Core3Angular8/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public Students: Student[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    console.log(baseUrl);
+    http.get<Student[]>(baseUrl + 'api/students').subscribe(result => {
+      console.log(result);
+      this.Students = result;
+    }, error => console.error(error));
+  }
+
+}
+
+
+interface Student {
+  id: number;
+  name: string;
+  roll: number;
+
 }
